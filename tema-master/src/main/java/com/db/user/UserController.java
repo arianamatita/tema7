60dfeff3ae45a59fd7a943957b96c54bbed92916
Ariana-Matita <arianamatita@gmail.com>
2022-04-14 23:16:31 +0300

    tema8
    Signed-off-by: Ariana-Matita <arianamatita@gmail.com>

--- tema-master/src/main/java/com/db/user/UserController.java
+++ tema-master/src/main/java/com/db/user/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class UserController {
@@ -33,5 +35,64 @@ public class UserController {
         if (userRepository.findByEmail(user.getEmail()) == null) {
             userRepository.save(user);
         } else throw new InvalidUserException("Invalid!");
+    }
+
+    @GetMapping("/initials")
+    public Stream<String> allUsersInitials() {
+        List<String> initials = new ArrayList<>();
+        Iterable<User> users = userRepository.findAll();
+        for (User user : users) {
+            initials.add(user.getFirstName() + " " + user.getLastName());
+        }
+        return initials.stream().map(this::getInitials);
+    }
+
+    @GetMapping("/gmail")
+    public long getNumberByGmail() {
+        List<User> users = (List<User>) userRepository.findAll();
+        return users.stream().filter(user -> user.getEmail().contains("@gmail")).count();
+    }
+
+    @GetMapping("/containsA")
+    public long getNumberByFirstName() {
+        List<User> users = (List<User>) userRepository.findAll();
+        return users.stream().filter(user -> (user.getFirstName().contains("A") ||
+                user.getFirstName().contains("a")) && user.getAge() < 20).count();
+    }
+
+    @GetMapping("/FirstNameInitials")
+    public String getAllUsersFirstNameInitials() {
+        List<User> users = (List<User>) userRepository.findAll();
+        List<String> initials = users.stream().map(User::getFirstName)
+                .collect(Collectors.toList());
+        String joined = initials.stream().map(this::getFirstNameInitials)
+                .collect(Collectors.joining());
+        return joined;
+    }
+
+    @GetMapping("list")
+    public List<String> listOfEachLastName() {
+        List<User> users = (List<User>) userRepository.findAll();
+        List<String> list = users.stream().map(User::getLastName)
+                .distinct().collect(Collectors.toList());
+        return list;
+    }
+
+
+    private String getFirstNameInitials(String name) {
+        String[] words = name.split(" ");
+        name = String.valueOf(Character.toUpperCase(name.charAt(0)));
+        return name;
+    }
+
+    private String getInitials(String name) {
+        String[] words = name.split(" ");
+        name = String.valueOf(Character.toUpperCase(
+                name.charAt(0)));
+        String name1 = "";
+        name1 = String.valueOf(Character.toUpperCase(
+                words[1].charAt(0)));
+        name = name.concat(name1);
+        return name;
     }
 }
